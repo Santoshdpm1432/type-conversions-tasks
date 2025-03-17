6e9582f9db004bcb10e0202e957d436787f8d7ca
santosh kumar agepogu <santoshkumaragepogu484@gmail.com>
2025-03-17 00:51:44 -0600

    Add files via upload

---
+++ type_conversion.py
@@ -0,0 +1,65 @@
+#int----------->float
+# number_1 = 12
+# print(type(number_1))
+# float_con = float(number_1)
+# print(type(float_con))
+
+#float----------->int
+# height = 5.6
+# print(type(height))
+# int_con = float(height)
+# print(type(int_con))
+
+#int----------->string
+# number_1 = 12
+# print(type(number_1))
+# string_con = str(number_1)
+# print(type(string_con))
+
+
+#float----------->string
+# height = 5.6
+# print(type(height))
+# string_con = str(height)
+# print(type(string_con))
+# print(string_con)
+
+#string----------->int
+# weight = "56"
+# print(type(weight))
+# int_con = int(weight)
+# print(type(int_con))
+
+#string----------->float
+# Height = "5.6"
+# print(type(Height))
+# float_con = float(Height)
+# print(type(float_con))
+
+# list-------------->set
+
+# list_1 = [1,2,3,4,(1,2,3),"santosh","ashok","nagaprasad","ashok"]
+# list_2 = set(list_1)
+# print(list_2)
+# print(type(list_2))
+
+#list-------------->tuple
+
+# list_1 = [1,2,3,4,(1,2,3),"santosh","ashok","nagaprasad","ashok"]
+# list_2 = tuple(list_1)
+# print(list_2)
+# print(type(list_2))
+
+#implicint type casting
+
+# num_1 = 10
+# num_2 = 20.5
+# addition = num_1 + num_2
+# print(addition)
+# print(type(addition))
+
+#input function
+# num_1 = int(input("enter number one :"))
+# num_2 = int(input("enter number two :"))
+# addition = num_1 + num_2
+# print(addition)
